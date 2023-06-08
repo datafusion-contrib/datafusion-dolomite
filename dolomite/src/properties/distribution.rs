9a94945e053f8444b465b36e4436c56a0f6475db
Renjie Liu <liurenjie2008@gmail.com>
2023-06-08 15:40:44 +0800

    Revert join types

--- dolomite/src/properties/distribution.rs
+++ dolomite/src/properties/distribution.rs
@@ -1,26 +1,20 @@
-use crate::properties::DistributionSpec::Random;
 use datafusion::prelude::Column;
 
 use crate::properties::PhysicalProp;
 
-#[derive(Hash, Debug, Clone, Eq, PartialEq)]
+#[derive(Hash, Debug, Clone, Eq, PartialEq, Default)]
 pub enum DistributionSpec {
     /// The data set is not partitioned and has only one partition.
     Singleton,
     /// The data set is partitioned according to hash values of columns.
     Hashed(Vec<Column>),
     /// The data set has several partitions, but the partitioning doesn't following any rule.
+    #[default]
     Random,
 }
 
 impl PhysicalProp for DistributionSpec {
     fn satisfies(&self, _other: &Self) -> bool {
         true
-    }
-}
-
-impl Default for DistributionSpec {
-    fn default() -> Self {
-        Random
     }
 }
